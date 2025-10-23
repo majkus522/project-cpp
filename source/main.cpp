99dbdfe8e887277321e63a39baad02dc60f9ac5e
majkus522 <mikohus522@gmail.com>
2025-10-23 08:38:21 +0200

    Remove debug (cout)

--- source/main.cpp
+++ source/main.cpp
@@ -17,37 +17,25 @@ const int n = 15;
 
 void drawGrid(RenderWindow *window, Cell **cells)
 {
-	cout << "d" << endl;
     window->clear();
-	cout << "d" << endl;
     Vector2u size = window->getSize();
-	cout << cells[7][7].state << endl;
     for (int y = 0; y * gridSize + (y + 1) * padding < size.y & y < n; y++)
     {
         for (int x = 0; x * gridSize + (x + 1) * padding < size.x & x < n; x++)
         {
-			cout << "a" << endl;
             RectangleShape shape({gridSize, gridSize});
-			cout << "a" << endl;
             shape.setPosition(x * gridSize + (x + 1) * padding, y * gridSize + (y + 1) * padding);
-			cout << "a" << endl;
             shape.setFillColor(cells[y][x].getColor());
-			cout << "a" << endl;
             window->draw(shape);
-			cout << "a" << endl;
         }
     }
     window->display();
-	cout << "d" << endl;
 }
 
 int main()
 {
-	cout << "k" << endl;
     RenderWindow window(VideoMode::getDesktopMode(), "SFML works!");
-	cout << "k" << endl;
     Cell **cells = new Cell*[n];
-	cout << "k" << endl;
     for (int y = 0; y < n; y++)
     {
 		cells[y] = (Cell*)malloc(sizeof(Cell) * n);
@@ -56,11 +44,8 @@ int main()
             cells[y][x] = NormalCell();
         }
     }
-	cout << "k" << endl;
     cells[7][7] = SickCell();
-	cout << "k" << endl;
     drawGrid(&window, cells);
-	cout << "k" << endl;
     Clock clock;
     Time delay = seconds(5);
     clock.restart();
